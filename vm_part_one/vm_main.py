d1e64bba3388770967ebe10167f250972af2c288
sammydowds <sammydowds1993@gmail.com>
2020-09-22 09:23:04 -0500

    Adding read file function

--- vm_part_one/vm_main.py
+++ vm_part_one/vm_main.py
@@ -2,10 +2,18 @@
 #INPUT = .vm file 
 #OUTPUT = .asm file 
 #TESTING: using the VM emulator using the supplied XxxVME.tst script
-#IMPLEMENTATION: 9 arithmetic / logical commands of the VM language as well as the VM command push constant x.  
+#IMPLEMENTATION: 9 arithmetic / logical commands of the VM language as well as the VM command push constant x. Add comment to output file for each command for help with debugging. 
+#ARITH/LOGIC Commands:
+#add, sub, neg, eq, gt, lt, and, or, not
+#MEM Commands: 
+#pop segment i, push segment i    
 
-def open_file(file): 
+url_file = 'C:\\Users\\sammy\\Desktop\\nand2tetris\\projects\\07\\MemoryAccess\\BasicTest\\BasicTest.vm'
+
+def open_file(file):
+    f = open(file, 'r')
+    print(f.read()) 
     return file
 
-
+open_file(url_file)
 
